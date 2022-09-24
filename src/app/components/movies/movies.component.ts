fa7c7f6079c7237325609dc4ddec2bc99416dd84
Ahmed Sameh <jr5sameh@gmail.com>
2022-09-24 02:44:51 +0200

    movies added

--- src/app/components/movies/movies.component.ts
+++ src/app/components/movies/movies.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Movie } from 'src/app/core/models/movie.model';
+import { ErrorHandlingService } from 'src/app/core/services/error-handling/error-handling.service';
+import { MoviesService } from 'src/app/core/services/movies/movies.service';
 
 @Component({
   selector: 'app-movies',
@@ -6,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movies.component.css']
 })
 export class MoviesComponent implements OnInit {
-
-  constructor() { }
+  movies: Movie[] = []
+  constructor(private moviesService: MoviesService, private errorService: ErrorHandlingService) { }
 
   ngOnInit(): void {
+    this.getAllMovies()
+  }
+
+  getAllMovies() {
+    this.moviesService.getAllMovies().subscribe({
+      next: res => this.movies = res.message,
+      error: error => this.errorService.errorOccured.next(error.message)
+    })
   }
 
 }
--- src/app/core/services/movies/movies.service.ts
+++ src/app/core/services/movies/movies.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpService } from '../http/http.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MoviesService {
 
-  constructor() { }
+  constructor(private http: HttpService) { }
+
+  getAllMovies() {
+    return this.http.get('movies')
+  }
 }
